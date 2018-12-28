0257a6e94bbadf43c931453642eab434dd4a7772
Chris Langton <chris.langton@cmdsolutions.com.au>
2018-12-28 17:55:24 +1100

    refactor to support awscli and added argparse

--- src/helpers/__init__.py
+++ src/helpers/__init__.py
@@ -1,4 +1,4 @@
-import os, logging, colorlog, boto3, socket, logging
+import sys, os, logging, colorlog, boto3, socket, logging, configparser
 from os import path, getcwd, environ
 from yaml import load, dump
 from jinja2 import Environment, FileSystemLoader
@@ -39,10 +39,26 @@ def make_arn(service, resource, partition='aws', use_account_id=True, account_id
   )
 
 
-def start_boto_session(profile=None, region=None):
+def start_boto_session(credentials_file=None, temp_profile=None, access_key_id=None, secret_access_key=None, profile=None, region=None):
   global session
+  if (access_key_id and not secret_access_key) or \
+     (secret_access_key and not access_key_id):
+    log.error('Set both secret_access_key and access_key_id together')
+    sys.exit(1)
+
   if not session:
-    session = boto3.Session(profile_name=profile, region_name=region)
+    session = boto3.Session(profile_name=profile, region_name=region,
+                            aws_access_key_id=access_key_id,
+                            aws_secret_access_key=secret_access_key)
+    if not profile:
+      if not credentials_file or not temp_profile:
+        log.error('when not using a profile ensure credentials_file and temp_profile are defined')
+        sys.exit(1)
+      update_config(credentials_file,
+                    profile=temp_profile,
+                    region=region,
+                    access_key_id=access_key_id,
+                    secret_access_key=secret_access_key)
 
 
 def get_aws_account_id(return_identity=False):
@@ -51,12 +67,12 @@ def get_aws_account_id(return_identity=False):
 
   identity = sts_client.get_caller_identity()
   if return_identity:
-    return identity["Account"], identity
+    return identity['Account'], identity
   
-  return identity["Account"]
+  return identity['Account']
 
 
-def assume_role(role, region=None, profile=None):
+def assume_role(role, temp_profile, credentials_file, region=None, profile=None):
   global session
   assume_session = boto3.Session(profile_name=profile, region_name=region)
   sts_client = assume_session.client('sts')
@@ -66,7 +82,7 @@ def assume_role(role, region=None, profile=None):
     role_arn = role
   else:
     role_session_name = role
-    role_arn = make_arn('iam', 'role/%s' % role)
+    role_arn = make_arn('iam', path.join('role', role))
 
   assumedRoleObject = sts_client.assume_role(
     RoleArn=role_arn,
@@ -79,10 +95,43 @@ def assume_role(role, region=None, profile=None):
                        aws_secret_access_key=credentials['SecretAccessKey'],
                        aws_session_token=credentials['SessionToken'],
                        region_name=region)
+  update_config(credentials_file,
+                profile=temp_profile,
+                region=region,
+                role_arn=role_arn,
+                access_key_id=credentials['AccessKeyId'],
+                secret_access_key=credentials['SecretAccessKey'],
+                session_token=credentials['SessionToken'],
+                expiration=credentials['Expiration'])
 
-def get_config():
+
+def update_config(credentials_file, profile, access_key_id=None, secret_access_key=None, session_token=None, role_arn=None, region=None, expiration=None):
+  log.info('Updating aws credentials file with profile [%s]' % profile)
+  config = configparser.RawConfigParser()
+  with open(credentials_file, 'r') as f:
+    config.readfp(f)
+    config.remove_section(profile)
+
+  with open(credentials_file, 'w') as f:
+    config.add_section(profile)
+    config.set(profile, 'output', 'json')
+    config.set(profile, 'aws_access_key_id', access_key_id)
+    config.set(profile, 'aws_secret_access_key', secret_access_key)
+    if session_token:
+      config.set(profile, 'aws_session_token', session_token)
+    if expiration:
+      config.set(profile, 'expiration', expiration)
+    if role_arn:
+      config.set(profile, 'aws_role_arn', role_arn)
+    if region:
+      config.set(profile, 'region', region)
+    config.write(f)
+
+
+def get_config(config_file=None):
   global config
-  config_file = 'config.yaml'
+  if not config_file:
+    config_file = 'config.yaml'
   config_path = path.realpath(getcwd())
   if not config:
     env = Environment(loader = FileSystemLoader(config_path), trim_blocks=True, lstrip_blocks=True)
@@ -98,7 +147,7 @@ def get_config():
 
   return config
 
-def get_session():
+def get_session(temp_profile, credentials_file):
   global session
   
   config = get_config()
@@ -106,8 +155,8 @@ def get_session():
   aws_region = config['aws'].get('region')
   aws_profile = config['aws'].get('profile')
 
-  start_boto_session(profile=aws_profile, region=aws_region)
+  start_boto_session(temp_profile=temp_profile, credentials_file=credentials_file, profile=aws_profile, region=aws_region)
   if 'assume_role' in config['aws']:
-    assume_role(config['aws']['assume_role'], region=aws_region, profile=aws_profile)
+    assume_role(config['aws']['assume_role'], temp_profile=temp_profile, credentials_file=credentials_file, region=aws_region, profile=aws_profile)
 
   return session
--- src/main.py
+++ src/main.py
@@ -1,34 +1,55 @@
-import logging
-from helpers import get_session, get_config
+import logging, argparse
+from helpers import make_arn, get_session, get_config, start_boto_session
+from os import path
+from subprocess import check_output
+from os.path import expanduser
 
-def main():
+
+def main(credentials_file, temp_profile, log_level, config_file=None):
   log = logging.getLogger('s4tool')
-  log.info('do argparse')
+  log_levels = {'DEBUG': logging.DEBUG,
+                'INFO': logging.INFO,
+                'WARNING': logging.WARNING,
+                'WARN': logging.WARN,
+                'ERROR': logging.ERROR,
+                'CRITICAL': logging.CRITICAL}
 
+  log.setLevel(log_levels[log_level])
 
-def setup():
-  log = logging.getLogger('s4tool')
-  session = get_session()
-  config = get_config()
-  s3_client = session.client('s3')
+  config = get_config(config_file)
+  aws_region = config['aws'].get('region')
+  session = get_session(temp_profile, credentials_file)
+  profile = config['aws'].get('profile')
+  if not profile or config['aws'].get('assume_role'):
+    profile = temp_profile
+
+  # print check_output(['aws', '--profile', profile, 's3', 'ls'])
+  kms = session.client('kms')
+  alias = config['kms']['alias']
+  if alias.startswith('arn:aws'):
+    alias_arn = alias
+  else:
+    alias_arn = make_arn('kms', path.join('alias', alias), region=aws_region)
+  log.info(alias_arn)
+  log.info('gathering a list of KMS aliases')
+  for k in kms.list_aliases()['Aliases']:
+    log.info('comparing %s' % k['AliasArn'])
+    if alias_arn == k['AliasArn']:
+      log.info('found %s' % alias_arn)
+
+  s3 = session.client('s3')
   log.info('gathering a list of S3 buckets')
-  for b in s3_client.list_buckets()['Buckets']:
+  for b in s3.list_buckets()['Buckets']:
     if b['Name'] == config['s3']['bucket']:
       log.info('bucket [%s] exists' % b['Name'])
 
-
-def sync():
-  log = logging.getLogger('s4tool')
-  session = get_session()
-  config = get_config()
-  s3_client = session.client('s3')
-  log.info('gathering a list of S3 buckets')
-  for b in s3_client.list_buckets()['Buckets']:
-    if b['Name'] == config['s3']['bucket']:
-      log.info('found bucket [%s]' % b['Name'])
-
-
 if __name__ == '__main__':
-  main()
-  setup()
-  sync()
+  CREDS_FILE='%s/.aws/credentials' % expanduser("~")
+  parser = argparse.ArgumentParser(description='s4tool')
+  parser.add_argument('-f', '--credentials_file', default=CREDS_FILE, help='absolute path to aws credentials file (default: %s)' % CREDS_FILE)
+  parser.add_argument('-c', '--config_file', default=None, help='absolute path to s4tool config file')
+  parser.add_argument('-p', '--temp_profile', type=str, default='s4tool', help='s4tool will save credentials to leverage the awscli')
+  parser.add_argument('-l', '--log_level', type=str, default='INFO')
+  args = parser.parse_args()
+
+  main(**args.__dict__)
